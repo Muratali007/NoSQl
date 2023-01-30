1d6d44bcab082104c80033526dc54c9aada0040d
muratali <amangalimuratali@gmail.com>
2023-01-30 18:50:51 +0600

    sa

--- Handlers/handler.go
+++ Handlers/handler.go
@@ -1,22 +1,16 @@
 package Handlers
 
 import (
+	"MongoDb/internal/data"
 	"MongoDb/pkg/logging"
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"net/http"
 	"time"
 )
-
-var gmail string
-
-type User struct {
-	Name     string
-	Email    string
-	Password string
-}
 
 func Register(w http.ResponseWriter, r *http.Request) {
 	logger := logging.GetLogger()
@@ -33,7 +27,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 		var count int64
 		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-		count, err := users.CountDocuments(ctx, bson.M{"email": email})
+		count, err := data.Collection.CountDocuments(ctx, bson.M{"email": email})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -50,7 +44,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		}
 
 		ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
-		_, err = users.InsertOne(ctx, bson.M{"email": email, "name": name, "password": hashedPassword})
+		_, err = data.Collection.InsertOne(ctx, bson.M{"email": email, "name": name, "password": hashedPassword})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -63,13 +57,129 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("html/register.html"))
 	tmpl.Execute(w, nil)
 }
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	logger := logging.GetLogger()
+
+	if r.Method == http.MethodPost {
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+
+		if email == "" || password == "" || name == "" {
+			http.Error(w, "Name, email, and password are required", http.StatusBadRequest)
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		result := data.Collection.FindOneAndUpdate(
+			ctx,
+			bson.M{"email": email},
+			bson.M{
+				"$set": bson.M{
+					"name":     name,
+					"password": hashedPassword,
+				},
+			},
+			options.FindOneAndUpdate().SetReturnDocument(options.After),
+		)
+		if result.Err() != nil {
+			http.Error(w, result.Err().Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var user data.User
+		err = result.Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.SetCookie(w, &http.Cookie{
+			Name:    "session",
+			Value:   user.Email,
+			Expires: time.Now().Add(24 * time.Hour),
+		})
+
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+		logger.Infof("%s updated profile", email)
+		return
+	}
+
+	email, err := getEmailFromCookie(r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	var user data.User
+	err = data.Collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("html/update.html"))
+	tmpl.Execute(w, user)
+}
+
+func getEmailFromCookie(r *http.Request) (string, error) {
+	cookie, err := r.Cookie("session")
+	if err != nil {
+		return "", err
+	}
+	return cookie.Value, nil
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	logger := logging.GetLogger()
+
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+
+		if email == "" {
+			http.Error(w, "Email is required", http.StatusBadRequest)
+			return
+		}
+
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+		// Check if the user exists
+		var result data.User
+		err := data.Collection.FindOne(ctx, bson.M{"email": email}).Decode(&result)
+		if err != nil {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+
+		// Delete the user
+		_, err = data.Collection.DeleteOne(ctx, bson.M{"email": email})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		logger.Infof("%s deleted their account", email)
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("html/delete.html"))
+	tmpl.Execute(w, nil)
+}
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	logger := logging.GetLogger()
 	if r.Method == http.MethodPost {
 
 		name := r.FormValue("name")
-		gmail = name
+		data.Gmail = name
 		email := r.FormValue("email")
 		password := r.FormValue("password")
 
@@ -78,15 +188,15 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var result User
+		var result data.User
 		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-		err := users.FindOne(ctx, bson.M{"email": email}).Decode(&result)
+		err := data.Collection.FindOne(ctx, bson.M{"email": email}).Decode(&result)
 		if err != nil {
 			http.Error(w, "Invalid email", http.StatusUnauthorized)
 			return
 		}
 
-		err = users.FindOne(ctx, bson.M{"name": name}).Decode(&result)
+		err = data.Collection.FindOne(ctx, bson.M{"name": name}).Decode(&result)
 		if err != nil {
 			http.Error(w, "Invalid name", http.StatusUnauthorized)
 			return
@@ -122,25 +232,14 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 	})
 
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
-	logger.Infof("%s do log out", gmail)
-	gmail = ""
-}
-func getUser(w http.ResponseWriter) User {
-	name := gmail
-	var result User
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	logger.Infof("%s do log out", data.Gmail)
+	data.Gmail = ""
 
-	err := users.FindOne(ctx, bson.M{"name": name}).Decode(&result)
-	if err != nil {
-		http.Error(w, "Error retrieving data from MongoDB", http.StatusInternalServerError)
-		return result
-	}
-	return result
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("html/home.html"))
-	tmpl.Execute(w, getUser(w))
+	tmpl.Execute(w, data.GetUser(w))
 }
 
 func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
@@ -154,7 +253,7 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 
 		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 		var count int64
-		count, err = users.CountDocuments(ctx, bson.M{"email": cookie.Value})
+		count, err = data.Collection.CountDocuments(ctx, bson.M{"email": cookie.Value})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
---
+++ internal/data/shop.go
@@ -0,0 +1,198 @@
+package data
+
+import (
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
+)
+
+type Cpu struct {
+	ID             string    `bson:"_id" json:"id,omitempty"`
+	Manufacturer   string    `bson:"manufacturer" json:"manufacturer,omitempty"`
+	Model          string    `bson:"model" json:"model,omitempty"`
+	Cores          int       `bson:"cores" json:"cores,omitempty"`
+	Threads        int       `bson:"threads" json:"threads,omitempty"`
+	ClockFrequency []float64 `bson:"clock_frequency" json:"clock_frequency,omitempty"`
+	Socket         string    `bson:"socket" json:"socket,omitempty"`
+	Ram            ramCpu    `bson:"ram"`
+	Tdp            int       `bson:"tdp" json:"tdp,omitempty"`
+	PciE           int       `bson:"pci-e" json:"pci_e,omitempty"`
+	Image          string    `bson:"image"`
+}
+
+type ramCpu struct {
+	Channels     int      `bson:"channels"`
+	Types        []string `bson:"types"`
+	MaxFrequency []int    `bson:"max_frequency"`
+	MaxCapacity  int      `bson:"max_capacity"`
+}
+
+type Motherboard struct {
+	ID           string     `bson:"_id"`
+	Manufacturer string     `bson:"manufacturer"`
+	Model        string     `bson:"model"`
+	Socket       string     `bson:"socket"`
+	Chipset      string     `bson:"chipset"`
+	FormFactor   string     `bson:"form_factor"`
+	Ram          ramMb      `bson:"ram"`
+	Interfaces   interfaces `bson:"interfaces"`
+	PciStandard  int        `bson:"pci_standard"`
+	MbPower      int        `bson:"mb_power"`
+	CpuPower     int        `bson:"cpu_power"`
+	Image        string     `bson:"image"`
+}
+
+type ramMb struct {
+	Slots        int    `bson:"slots"`
+	Type         string `bson:"type"`
+	MaxFrequency int    `bson:"max_frequency"`
+	MaxCapacity  int    `bson:"max_capacity"`
+}
+
+type interfaces struct {
+	Sata3   int `bson:"SATA3"`
+	M2      int `bson:"M2"`
+	PciE1x  int `bson:"PCI_E_1x"`
+	PciE16x int `bson:"PCI_E_16x"`
+}
+
+type Ram struct {
+	ID           string  `bson:"_id"`
+	Manufacturer string  `bson:"manufacturer"`
+	Model        string  `bson:"model"`
+	Capacity     int     `bson:"capacity"`
+	Number       int     `bson:"number"`
+	FormFactor   string  `bson:"form_factor"`
+	Rank         int     `bson:"rank"`
+	Type         string  `bson:"type"`
+	Frequency    int     `bson:"frequency"`
+	Bandwidth    int     `bson:"bandwidth"`
+	CasLatency   string  `bson:"cas_latency"`
+	TimingScheme []int   `bson:"timing_scheme"`
+	Voltage      float64 `bson:"voltage"`
+	Cooling      string  `bson:"cooling"`
+	Height       int     `bson:"height"`
+}
+
+type Ssd struct {
+	ID           string  `bson:"_id"`
+	Manufacturer string  `bson:"manufacturer"`
+	Model        string  `bson:"model"`
+	Type         string  `bson:"type"`
+	Capacity     int     `bson:"capacity"`
+	Interface    string  `bson:"interface"`
+	MemoryType   string  `bson:"memory_type"`
+	Read         int     `bson:"read"`
+	Write        int     `bson:"write"`
+	FormFactor   string  `bson:"form_factor"`
+	Mftb         float64 `bson:"mftb"`
+	Size         []int   `bson:"size"`
+	Weight       int     `bson:"weight"`
+}
+
+type Hdd struct {
+	ID           string `bson:"_id"`
+	Manufacturer string `bson:"manufacturer"`
+	Model        string `bson:"model"`
+	Type         string `bson:"type"`
+	Capacity     int    `bson:"capacity"`
+	Interface    string `bson:"interface"`
+	WriteMethod  string `bson:"write_method"`
+	TransferRate int    `bson:"transfer_rate"`
+	SpindleSpeed int    `bson:"spindle_speed"`
+	FormFactor   string `bson:"form_factor"`
+	Mftb         int    `bson:"mftb"`
+	Size         []int  `bson:"size"`
+	Weight       int    `bson:"weight"`
+}
+
+type Gpu struct {
+	ID            string          `bson:"_id"`
+	Manufacturer  string          `bson:"manufacturer"`
+	Model         string          `bson:"model"`
+	Architecture  string          `bson:"architecture"`
+	Memory        memoryGpu       `bson:"memory"`
+	GpuFrequency  int             `bson:"gpu_frequency"`
+	ProcessSize   int             `bson:"process_size"`
+	MaxResolution string          `bson:"max_resolution"`
+	Interfaces    []interfacesGpu `bson:"interfaces"`
+	MaxMonitors   int             `bson:"max_monitors"`
+	Cooling       coolingGpu      `bson:"cooling"`
+	Tdp           int             `bson:"tdp"`
+	TdpR          int             `bson:"tdp_r"`
+	PowerSupply   []int           `bson:"power_supply"`
+	Slots         float64         `bson:"slots"`
+	Size          []int           `bson:"size"`
+	Image         string          `bson:"image"`
+}
+
+type memoryGpu struct {
+	Capacity       int    `bson:"capacity"`
+	Type           string `bson:"type"`
+	InterfaceWidth int    `bson:"interface_width"`
+	Frequency      int    `bson:"frequency"`
+}
+
+type interfacesGpu struct {
+	Type   string `bson:"type"`
+	Number int    `bson:"number"`
+}
+
+type coolingGpu struct {
+	Type      string `bson:"type"`
+	FanNumber int    `bson:"fan_number"`
+}
+
+type Cooling struct {
+	ID           string   `bson:"_id"`
+	Manufacturer string   `bson:"manufacturer"`
+	Model        string   `bson:"model"`
+	Type         string   `bson:"type"`
+	Sockets      []string `bson:"sockets"`
+	Fans         []int    `bson:"fans"`
+	Rpm          []int    `bson:"rpm"`
+	Tdp          int      `bson:"tdp"`
+	NoiseLevel   int      `bson:"noise_level"`
+	MountType    string   `bson:"mount_type"`
+	Power        int      `bson:"power"`
+	Height       int      `bson:"height"`
+	Image        string   `bson:"image"`
+}
+
+type Housing struct {
+	ID              string    `bson:"_id"`
+	Manufacturer    string    `bson:"manufacturer"`
+	Model           string    `bson:"model"`
+	FormFactor      string    `bson:"form_factor"`
+	DriveBays       driveBays `bson:"drive_bays"`
+	MbFormFactor    string    `bson:"mb_form_factor"`
+	PsFormFactor    string    `bson:"ps_form_factor"`
+	ExpansionSlots  int       `bson:"expansion_slots"`
+	GraphicCardSize int       `bson:"graphic_card_size"`
+	CoolerHeight    int       `bson:"cooler_height"`
+	Size            []int     `bson:"size"`
+	Weight          float64   `bson:"weight"`
+	Image           string    `bson:"image"`
+}
+
+type driveBays struct {
+	D35 int `bson:"3_5"`
+	D25 int `bson:"2_5"`
+}
+
+type PowerSupply struct {
+	ID           string         `bson:"_id"`
+	Manufacturer string         `bson:"manufacturer"`
+	Model        string         `bson:"model"`
+	FormFactor   string         `bson:"form_factor"`
+	OutputPower  int            `bson:"output_power"`
+	Connectors   connectors     `bson:"connectors"`
+	Modules      bool           `bson:"modules"`
+	MbPower      int            `bson:"mb_power"`
+	CpuPower     bsoncore.Array `bson:"cpu_power"`
+	Image        string         `bson:"image"`
+}
+
+type connectors struct {
+	Sata  int   `bson:"SATA"`
+	Molex int   `bson:"MOLEX"`
+	PciE  []int `bson:"PCI_E"`
+}
